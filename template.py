9c2a80c850eae2fda310e3d0ad81dbbd80d0b625
Henry Goodman <13032204@student.uts.edu.au>
2022-12-03 10:32:11 +1100

    Update template, add input data to gitignore

---
+++ template.py
@@ -0,0 +1,17 @@
+file1 = open('input.txt', 'r')
+lines = file1.readlines()
+
+# Part 1
+
+def part1():
+    for line in lines:
+        pass
+    return
+
+# Part 2
+def part2():
+    for line in lines:
+        pass
+    return
+
+print(part1(), part2())
