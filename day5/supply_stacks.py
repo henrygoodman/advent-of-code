da85a9277756560204c43ee70266f166637500b5
Henry Goodman <13032204@student.uts.edu.au>
2022-12-05 18:26:00 +1100

    Use copy module to do deep copy of 2d list

--- day5/supply_stacks.py
+++ day5/supply_stacks.py
@@ -1,32 +1,25 @@
+import copy
+
 file1 = open('input.txt', 'r')
 lines = file1.readlines()
 
 # Manually store the input data in a 2d list, treat them as queues.
 # For some reason Python doesnt like copying lists (like with .copy() or list())
-stacks = [  ['N','D','M','Q','B','P','Z'],
-            ['C' ,'L' ,'Z' ,'Q' ,'M' ,'D' ,'H' ,'V'],
-            ['Q' ,'H' ,'R' ,'D' ,'V' ,'F' ,'Z' ,'G'],
-            ['H' ,'G' ,'D' ,'F' ,'N'],
-            ['N' ,'F' ,'Q'],
-            ['D' ,'Q' ,'V' ,'Z' ,'F' ,'B' ,'T'],
-            ['Q' ,'M' ,'T' ,'Z' ,'D' ,'V' ,'S' ,'H'],
-            ['M' ,'G' ,'F' ,'P' ,'N' ,'Q'],
-            ['B' ,'W' ,'R' ,'M']
-            ]
+stacks_original = [  ['N','D','M','Q','B','P','Z'],
+                    ['C' ,'L' ,'Z' ,'Q' ,'M' ,'D' ,'H' ,'V'],
+                    ['Q' ,'H' ,'R' ,'D' ,'V' ,'F' ,'Z' ,'G'],
+                    ['H' ,'G' ,'D' ,'F' ,'N'],
+                    ['N' ,'F' ,'Q'],
+                    ['D' ,'Q' ,'V' ,'Z' ,'F' ,'B' ,'T'],
+                    ['Q' ,'M' ,'T' ,'Z' ,'D' ,'V' ,'S' ,'H'],
+                    ['M' ,'G' ,'F' ,'P' ,'N' ,'Q'],
+                    ['B' ,'W' ,'R' ,'M']
+                    ]
 
-stacks2 = [ ['N','D','M','Q','B','P','Z'],
-            ['C' ,'L' ,'Z' ,'Q' ,'M' ,'D' ,'H' ,'V'],
-            ['Q' ,'H' ,'R' ,'D' ,'V' ,'F' ,'Z' ,'G'],
-            ['H' ,'G' ,'D' ,'F' ,'N'],
-            ['N' ,'F' ,'Q'],
-            ['D' ,'Q' ,'V' ,'Z' ,'F' ,'B' ,'T'],
-            ['Q' ,'M' ,'T' ,'Z' ,'D' ,'V' ,'S' ,'H'],
-            ['M' ,'G' ,'F' ,'P' ,'N' ,'Q'],
-            ['B' ,'W' ,'R' ,'M']
-            ]
 # Part 1
 
 def part1():
+    stacks = copy.deepcopy(stacks_original)
     for line in lines:
         line = [s for s in line.strip().split() if s.isdigit()]
         number = int(line[0])
@@ -42,6 +35,7 @@ def part1():
 
 # Part 2
 def part2():
+    stacks = copy.deepcopy(stacks_original)
     for line in lines:
         line = [s for s in line.strip().split() if s.isdigit()]
         number = int(line[0])
@@ -50,12 +44,12 @@ def part2():
         # To retain order, just pop twice using a temporary stack.
         sub_stack = []
         for _ in range(number):
-            sub_stack.append(stacks2[src-1].pop())
+            sub_stack.append(stacks[src-1].pop())
         for _ in range(number):
-            stacks2[dst-1].append(sub_stack.pop())
+            stacks[dst-1].append(sub_stack.pop())
 
     ret = []
-    for stack in stacks2:
+    for stack in stacks:
         ret.append(stack[-1])
     return ''.join(ret)
 
