311783d56871b1e8e8684b7ad9e773f53ce35915
Henry Goodman <13032204@student.uts.edu.au>
2022-12-05 16:53:33 +1100

    Supply Stacks Day 5

---
+++ day5/supply_stacks.py
@@ -0,0 +1,60 @@
+file1 = open('input.txt', 'r')
+lines = file1.readlines()
+
+# Manually store the input data in a 2d list, treat them as queues.
+# For some reason Python doesnt like copying lists (like with .copy() or list())
+stacks = [  ['N','D','M','Q','B','P','Z'],
+            ['C' ,'L' ,'Z' ,'Q' ,'M' ,'D' ,'H' ,'V'],
+            ['Q' ,'H' ,'R' ,'D' ,'V' ,'F' ,'Z' ,'G'],
+            ['H' ,'G' ,'D' ,'F' ,'N'],
+            ['N' ,'F' ,'Q'],
+            ['D' ,'Q' ,'V' ,'Z' ,'F' ,'B' ,'T'],
+            ['Q' ,'M' ,'T' ,'Z' ,'D' ,'V' ,'S' ,'H'],
+            ['M' ,'G' ,'F' ,'P' ,'N' ,'Q'],
+            ['B' ,'W' ,'R' ,'M']
+            ]
+
+stacks2 = [  ['N','D','M','Q','B','P','Z'],
+            ['C' ,'L' ,'Z' ,'Q' ,'M' ,'D' ,'H' ,'V'],
+            ['Q' ,'H' ,'R' ,'D' ,'V' ,'F' ,'Z' ,'G'],
+            ['H' ,'G' ,'D' ,'F' ,'N'],
+            ['N' ,'F' ,'Q'],
+            ['D' ,'Q' ,'V' ,'Z' ,'F' ,'B' ,'T'],
+            ['Q' ,'M' ,'T' ,'Z' ,'D' ,'V' ,'S' ,'H'],
+            ['M' ,'G' ,'F' ,'P' ,'N' ,'Q'],
+            ['B' ,'W' ,'R' ,'M']
+            ]
+# Part 1
+
+def part1():
+    for line in lines:
+        line = [s for s in line.strip().split() if s.isdigit()]
+        number = int(line[0])
+        src = int(line[1])
+        dst = int(line[2])
+        for _ in range(number):
+            stacks[dst-1].append(stacks[src-1].pop())
+    
+    ret = []
+    for stack in stacks:
+        ret.append(stack[-1])
+    return ''.join(ret)
+
+# Part 2
+def part2():
+    for line in lines:
+        line = [s for s in line.strip().split() if s.isdigit()]
+        number = int(line[0])
+        src = int(line[1])
+        dst = int(line[2])
+        # To retain order, we need to push the entire sub-array
+        sub_stack = stacks2[src-1][-number:][::-1]
+        for _ in range(number):
+            stacks2[dst-1].append(sub_stack.pop())
+            stacks2[src-1].pop()
+    ret = []
+    for stack in stacks2:
+        ret.append(stack[-1])
+    return ''.join(ret)
+
+print(part1(), "\n\n", part2())
