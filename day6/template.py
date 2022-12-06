e84cf6cbd15d5dfda4b4ceae9c84702456d8c60d
henrygoodman <13032204@student.uts.edu.au>
2022-12-06 16:22:08 +1100

    Re-arrange format Day 6

--- day6/template.py
+++ day6/template.py
@@ -1,13 +1,12 @@
 file1 = open('input.txt', 'r')
 lines = file1.readlines()
 
-# Part 1
-
 def calc(n, line):
     for i in range(len(line)):
         if len(set(list([line[i:i+n]][0]))) == n:
             return i + n
 
+# Part 1
 def part1():
     return calc(4, lines[0])
 
